458cd231ddfaef344174793b2cdb35cae7aa488b
hetguci-svo <sava19210@gmail.com>
2024-11-12 22:03:40 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+token = '7822647284:AAF4PJ5CsR0ctTiokgNUTzFAeEY1I4msQrI' #токен бота
+admin = 6936938054 #айди создателя
+owner = 6936938054
+vip = 6936938054 #айди
+adminestrator = 6936938054
+gay = 6936938054
+bot_name = 'Botgaygamekarina_BoT' #сюда username бота без @
